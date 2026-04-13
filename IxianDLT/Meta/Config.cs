079b16c38dd44e1afae05b05e79fcc583db92963
IxiAngel <33629337+IxiAngel@users.noreply.github.com>
2026-04-13 12:15:53 +0200

    xdc-0.9.8a

--- IxianDLT/Meta/Config.cs
+++ IxianDLT/Meta/Config.cs
@@ -103,7 +103,7 @@ namespace DLT
             public static ulong forceSyncToBlock = 0;
 
             // Read-only values
-            public static readonly string version = "xdc-0.9.8a-dev"; // DLT Node version
+            public static readonly string version = "xdc-0.9.8a"; // DLT Node version
 
             public static readonly string checkVersionUrl = "https://resources.ixian.io/update.txt";
             public static readonly int checkVersionSeconds = 6 * 60 * 60; // 6 hours
